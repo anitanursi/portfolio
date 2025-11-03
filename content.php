7d929c709305e724644562fee2307a124f3e5f78
21343018 <112265105+21343018@users.noreply.github.com>
2025-11-03 15:23:46 +0700

    deploy

--- content.php
+++
@@ -1,8 +0,0 @@
-<!-- agar dinamis, tidak perlu membuat layout lagi walau kita membuat halaman baru -->
-
-<?php
-if (!empty($_GET['page'])) {
-    include_once($_GET['page'] . ".php");
-} else {
-    include "home.php";
-}
